79a07670df16afe0d7d4d1c9c54113394bfe51d7
Maksim Skrypka <skrypka.m.l@gmail.com>
2023-04-20 11:27:35 +0300

    Initial Commit

---
+++ TestTaskApp/TestTaskAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskAppApp.swift
+//  TestTaskApp
+//
+//  Created by Maksim Skrypka on 20.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
